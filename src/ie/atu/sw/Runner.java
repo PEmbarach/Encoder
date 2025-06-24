25c65d82f6eceeb0778d9b0b2683992c4c7d6052
pablo <pablo@PabloEmbarach.Home>
2025-06-24 13:42:57 +0100

    First Commit

--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -3,9 +3,8 @@ package ie.atu.sw;
 public class Runner {
 
 	public static void main(String[] args) {
-		Encoder encoder = new Encoder();
-		String result = encoder.encode("test");
-		System.out.println(result);
+		Menu m = new Menu();
+		m.start();
 	}
 	
 }
